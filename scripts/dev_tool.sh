34ef6b195140e0d7282b0ea1fe4ce12e583b1514
zeinsshiri1984 <zeinsshiri@gmail.com>
2026-04-15 01:46:13 +0800

    f

--- scripts/dev_tool.sh
+++ scripts/dev_tool.sh
@@ -4,5 +4,5 @@ set -e
 #uv tool install aider-chat@latest --python "$(mise where python@3.12)"
 
 # bun/pnpm都不适合全局分发cli, 全局cli的运行时在mise config固定了一个lts版本
-npm install -g @anthropic-ai/claude-code
+curl -fsSL https://claude.ai/install.sh | bash
 npm install -g @openai/codex
