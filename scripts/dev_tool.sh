354357885627ce629eb3bf3960049ba9b2317f1c
zeinsshiri1984 <zeinsshiri@gmail.com>
2026-04-26 15:01:39 +0800

    add ccswitch

--- scripts/dev_tool.sh
+++ scripts/dev_tool.sh
@@ -6,3 +6,5 @@ set -e
 # bun/pnpm都不适合全局分发cli, 全局cli的运行时在mise config固定了一个lts版本
 curl -fsSL https://claude.ai/install.sh | bash
 npm install -g @openai/codex
+
+curl -fsSL https://github.com/SaladDay/cc-switch-cli/releases/latest/download/install.sh | bash
